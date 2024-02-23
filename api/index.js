027e332cab8b374f6036f26d53177d5b3b0b859d
484021 <bhoopalsanthosh@gmail.com>
2024-02-22 22:35:20 -0500

    feat: connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
-import { log } from 'console';
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("connected to mongodb");
+}).catch((err) =>{
+    console.log(err);
+});
 
 const app = express();
 
