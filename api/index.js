3d22e30fbc568a9e450ef4c6a5e42ccc1db38b7d
484021 <bhoopalsanthosh@gmail.com>
2024-02-23 10:18:04 -0500

    feat:created signup api routes

--- api/index.js
+++ api/index.js
@@ -2,7 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 dotenv.config();
+
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("connected to mongodb");
@@ -12,8 +14,11 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () =>{
     console.log('Server is listening on port 3000');
 })
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
