86f5e24350992b768220093db164be01b96bf10c
484021 <bhoopalsanthosh@gmail.com>
2024-02-27 02:14:58 -0500

    feat: set up signin backend(set up cookies, compared the sign in details with the user data in the database and returned a cookie and some more userdata back as the response which we can use in the front end to build out the ui with the userdata we grabbed from the database

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
@@ -13,8 +14,25 @@ export const signup = async (req, res, next) => {
     }
     catch (error){
         next(errorHandler(300, "Something went wrong"));
+    } 
+}
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try{
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) next(errorHandler(401, "Invalid credentials"));
+        const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET);
+        const {password: hashedPassword, ...rest} = validUser._doc;
+        const expiryDate = new Date(Date.now() + 3600000);
+
+        res
+        .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+        .status(200)
+        .json(rest)
+    } catch (error){
+        next(error);
     }
- 
-        
-    
 }
