2da9a4dad7322ee7fae3c80c0e84fab59716edc5
484021 <bhoopalsanthosh@gmail.com>
2024-02-29 13:19:41 -0500

    feat: sign in working and sign up

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 const handleChange = (e) =>{
   setFormData({ ...formData, [e.target.id]: e.target.value});
 }
@@ -31,6 +32,7 @@ const handleSubmit = async(e) => {
       setError(true);
       return;
     }
+    navigate('/')
   }catch (error){
     setLoading(false);
     setError(true);
@@ -62,7 +64,7 @@ const handleSubmit = async(e) => {
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
         <Link to="/sign-in">
-        <span className='text-blue-500'>Sign in</span>
+        <span className='text-blue-500'>Sign In</span>
         </Link>
         
       </div>
