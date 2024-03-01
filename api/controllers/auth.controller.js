112442acd4dbb39267dcceceedc99a164cb3b6cb
484021 <bhoopalsanthosh@gmail.com>
2024-03-01 01:46:31 -0500

    feat: added redux toolkit/ sign in page auth done. learned how to store states with redux toolkit need more practice

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -23,7 +23,7 @@ export const signin = async (req, res, next) => {
         const validUser = await User.findOne({ email });
         if (!validUser) return next(errorHandler(404, 'User not found'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if(!validPassword) next(errorHandler(401, "Invalid credentials"));
+        if(!validPassword) return next(errorHandler(401, "Invalid credentials"));
         const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET);
         const {password: hashedPassword, ...rest} = validUser._doc;
         const expiryDate = new Date(Date.now() + 3600000);
@@ -36,3 +36,4 @@ export const signin = async (req, res, next) => {
         next(error);
     }
 }
+
